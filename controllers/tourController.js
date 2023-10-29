5cbf7b47e5bb32b125cd75eadab1a5dd2577ddfb
Vigneshwaran E <vigneshwarane@VignesharansMBP.ht.home>
2023-10-29 11:52:51 -0400

    Implement CRUD

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,21 +23,36 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'Success',
-    requestedAT: req.requestTime,
-    // results: tours.length,
-    // data: { tours },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'Success',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'Success',
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'Success',
 
-    // data: { tour },
-  });
+      data: { tour },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 
   //OLD USING LOCAL FILE
   // const id = parseInt(req.params.id, 10);
@@ -85,20 +100,31 @@ exports.createTour = async (req, res) => {
   // );
 };
 
-exports.updateTour = (req, res) => {
-  // if (parseInt(req.params.id) > tours.length) {
-  //   return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  // }
-  res
-    .status(200)
-    .json({ status: 'success', data: { tour: '<Updated tour here...' } });
+exports.updateTour = async (req, res) => {
+  try {
+    // if (parseInt(req.params.id) > tours.length) {
+    //   return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+    // }
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ status: 'success', data: { tour } });
+  } catch (error) {
+    res.status(400).json({ status: 'Fail', message: error });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  // if (parseInt(req.params.id) > tours.length) {
-  //   return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  // }
-  res.status(204).json({ status: 'success', data: null });
+exports.deleteTour = async (req, res) => {
+  try {
+    // if (parseInt(req.params.id) > tours.length) {
+    //   return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+    // }
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (error) {
+    res.status(404).json({ status: 'Fail', message: error });
+  }
 };
 
 // module.exports = { getAllTours, getTour, createTour, updateTour, deleteTour };
